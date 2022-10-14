5ae92575b7d670574f6f48e36f6c2c2f3472b708
AnneKammenhed <anne.juriado@gmail.com>
2022-10-14 13:06:31 +0000

    add image filters

--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,22 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    image_filter_choices = [
+        ('_1977', '1977'), 
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'), 
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'), 
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'), 
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'), 
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'), 
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'), 
+        ('xpro2', 'X-pro II'),
+    ]
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -11,6 +27,9 @@ class Profile(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_oni8cm'
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
     )
 
     class Meta:
