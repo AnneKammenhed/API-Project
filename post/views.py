c53df9bc73c47728759c626e0b9a8aab80b50f94
AnneKammenhed <anne.juriado@gmail.com>
2022-10-31 06:52:34 +0000

    add filters to post app

--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post
 from .serializers import PostSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
@@ -14,9 +15,20 @@ class PostList(generics.ListCreateAPIView):
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend, 
     ]
-
+    """get the posts by users a user is following, get the posts a user liked, get posts owned by a user"""
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
     ordering_fields = [
         'likes_count',
         'comments_count',
